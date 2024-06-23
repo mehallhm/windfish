bbe66adb63b0540e772d23c25a21204a0ecfcf8e
mehallhm <michaelhmehall@gmail.com>
2024-06-22 23:05:42 -0600

    feat: cpu performance v1

--- agent/router/websockets.go
+++ agent/router/websockets.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"bufio"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 
+	moby "github.com/docker/docker/api/types"
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mehallhm/panamax/events"
@@ -48,6 +52,63 @@ func registerWebsockets(app *fiber.App, workspace *stacks.Workspace) *fiber.App 
 		}
 
 		fmt.Println("bye")
+	}))
+
+	ws.Get("/stats/:project", websocket.New(func(c *websocket.Conn) {
+		project := c.Params("project")
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		_ = project
+
+		go func(c *websocket.Conn) {
+			var (
+				mt  int
+				msg []byte
+				err error
+			)
+
+			for {
+				if mt, msg, err = c.ReadMessage(); err != nil {
+					log.Println("read error:", err)
+					break
+				}
+				log.Printf("recv: %s with mt %d", msg, mt)
+			}
+			fmt.Println("reader stopped")
+		}(c)
+
+		stats, err := workspace.DockerClient.ContainerStats(ctx, "bb2-busybox-1", true)
+		if err != nil {
+			panic(err)
+		}
+		defer stats.Body.Close()
+
+		scanner := bufio.NewScanner(stats.Body)
+		for scanner.Scan() {
+			line := scanner.Bytes()
+
+			var statsJSON moby.StatsJSON
+			err = json.Unmarshal(line, &statsJSON)
+			if err != nil {
+				fmt.Println("json err: ", err)
+				return
+			}
+
+			cpuDelta := float64(statsJSON.CPUStats.CPUUsage.TotalUsage) - float64(statsJSON.PreCPUStats.CPUUsage.TotalUsage)
+			systemDelta := float64(statsJSON.CPUStats.SystemUsage) - float64(statsJSON.PreCPUStats.SystemUsage)
+			numberOfCores := float64(statsJSON.CPUStats.OnlineCPUs)
+
+			cpuPercent := (cpuDelta / systemDelta) * numberOfCores * 100.0
+			fmt.Printf("CPU usage: %.2f%%\n", cpuPercent)
+
+			err = c.WriteJSON(statsJSON)
+			if err != nil {
+				fmt.Println("websocket err: ", err)
+				return
+			}
+		}
+
 	}))
 
 	return app
--- panel/src/pages/project.tsx
+++ panel/src/pages/project.tsx
@@ -1,11 +1,45 @@
+import { Separator } from "@kobalte/core/separator";
 import { useParams } from "@solidjs/router";
+import { createSignal } from "solid-js";
+import { LineChart } from "~/components/ui/Charts";
 
 export default function Page() {
   const params = useParams<{ project: string }>();
+  const socket = new WebSocket("ws://localhost:3000/ws/stats/1");
+  const [percentages, setPercentages] = createSignal([]);
+  socket.onmessage = (e) => {
+    const data = JSON.parse(e.data);
+    const cpuDelta =
+      data.cpu_stats.cpu_usage.total_usage -
+      data.precpu_stats.cpu_usage.total_usage;
+    const systemDelta =
+      data.cpu_stats.system_cpu_usage - data.precpu_stats.system_cpu_usage;
+    const numCores = data.cpu_stats.online_cpus;
+
+    const percent = (cpuDelta / systemDelta) * numCores * 100;
+    if (percentages().length < 60) {
+      setPercentages([
+        ...percentages(),
+        { x: percentages().length, y: percent },
+      ]);
+    } else {
+      let arr = [...percentages()];
+      arr.shift();
+      setPercentages([...arr, { x: percentages().length, y: percent }]);
+    }
+  };
 
   return (
     <div class="w-full space-y-4 p-8">
       <h2 class="text-2xl font-semibold text-primary">{params.project}</h2>
+      <div class="h-64 w-full">
+        <LineChart
+          data={{
+            labels: Array.from(Array(60).keys()),
+            datasets: [{ data: percentages(), fill: true, animation: false }],
+          }}
+        />
+      </div>
     </div>
   );
 }
